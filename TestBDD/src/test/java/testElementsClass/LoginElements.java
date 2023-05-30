14a8179e79ce562aae9eb728ebdd36a7ee5fa429
nafisyasar <nafisyasar@nafiss-mbp>
2023-05-30 17:29:49 -0400

    Second commit

--- TestBDD/src/test/java/testElementsClass/LoginElements.java
+++ TestBDD/src/test/java/testElementsClass/LoginElements.java
@@ -1,7 +1,11 @@
 package testElementsClass;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginElements {
 	
-	@FindBy(xpath =)
+	@FindBy(xpath = "//*[contains(@class,'ico-login')]")
+	public WebElement login;
 
 }
